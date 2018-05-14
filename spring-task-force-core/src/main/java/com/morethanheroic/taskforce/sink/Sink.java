24ad5852fab6e3584f18367a6cdfdd8237015408
Gyula Lakatos <gyula.lakatos1@ge.com>
2018-05-14 20:57:59 +0200

    Renamed the project from spring-task-force to java-task-force.

--- spring-task-force-core/src/main/java/com/morethanheroic/taskforce/sink/Sink.java
+++
@@ -1,6 +0,0 @@
-package com.morethanheroic.taskforce.sink;
-
-public interface Sink<INPUT> {
-
-    void consume(INPUT input);
-}
--- spring-task-force-core/src/main/java/com/morethanheroic/taskforce/task/Task.java
+++
@@ -1,9 +0,0 @@
-package com.morethanheroic.taskforce.task;
-
-import java.util.Optional;
-
-@FunctionalInterface
-public interface Task<INPUT, OUTPUT> {
-
-    Optional<OUTPUT> execute(INPUT input);
-}
