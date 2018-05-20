1281bc399ab5c0a2082ea73345f8b7493efbd829
Gyula Lakatos <gyula.lakatos1@ge.com>
2018-05-20 23:25:47 +0200

    Added a builder option in the JobTaskPhaseBuilder that accepts a TaskDescriptor.

--- java-task-force-core/src/main/java/com/morethanheroic/taskforce/job/builder/JobTaskPhaseBuilder.java
+++ java-task-force-core/src/main/java/com/morethanheroic/taskforce/job/builder/JobTaskPhaseBuilder.java
@@ -21,6 +21,19 @@ public class JobTaskPhaseBuilder<NEXT_INPUT> {
     private final List<TaskDescriptor<?, ?>> tasks = new ArrayList<>();
 
     /**
+     * Adds fully configured {@link Task} to the {@link com.morethanheroic.taskforce.job.Job}. The task is already
+     * wrapped into a {@link TaskDescriptor}.
+     *
+     * @param taskDescriptor the task descriptor that contains the task
+     * @return this builder
+     */
+    public <OUTPUT> JobTaskPhaseBuilder<OUTPUT> task(final TaskDescriptor<NEXT_INPUT, OUTPUT> taskDescriptor) {
+        tasks.add(taskDescriptor);
+
+        return (JobTaskPhaseBuilder<OUTPUT>) this;
+    }
+
+    /**
      * Adds a {@link Task} to the {@link com.morethanheroic.taskforce.job.Job}. The task will be run with parallelism
      * level 1 in it's own thread pool.
      *
@@ -28,13 +41,7 @@ public class JobTaskPhaseBuilder<NEXT_INPUT> {
      * @return this builder
      */
     public <OUTPUT> JobTaskPhaseBuilder<OUTPUT> task(final Task<NEXT_INPUT, OUTPUT> task) {
-        tasks.add(
-                TaskDescriptor.<NEXT_INPUT, OUTPUT>builder()
-                        .parallelismLevel(1)
-                        .maxQueueSize(10000)
-                        .task(task)
-                        .build()
-        );
+        asyncTask(task, 1);
 
         return (JobTaskPhaseBuilder<OUTPUT>) this;
     }
--- java-task-force-core/src/main/java/com/morethanheroic/taskforce/task/TaskDescriptor.java
+++ java-task-force-core/src/main/java/com/morethanheroic/taskforce/task/TaskDescriptor.java
@@ -3,11 +3,30 @@ package com.morethanheroic.taskforce.task;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * A task descriptor wraps a {@link Task} and define all of the runtime properties (like parallelism level etc.) of the
+ * wrapped task.
+ *
+ * @param <INPUT>  the input type of the wrapped task
+ * @param <OUTPUT> the output type of the wrapped task
+ */
 @Getter
 @Builder
 public class TaskDescriptor<INPUT, OUTPUT> {
 
+    /**
+     * the task that we are wrapping into this descriptor.
+     */
     private final Task<INPUT, OUTPUT> task;
+
+    /**
+     * How many threads do we want to use when running the wrapped task.
+     */
     private final int parallelismLevel;
+
+    /**
+     * How many items do we want in the queue that sits before the wrapped task. Setting this value too high can cause
+     * {@link OutOfMemoryError}s. It should be 0 or a higher than 0 positive integer.
+     */
     private final int maxQueueSize;
 }
