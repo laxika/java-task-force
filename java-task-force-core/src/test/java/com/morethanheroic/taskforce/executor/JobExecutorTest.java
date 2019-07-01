6f27f9523bcb3f316921fa2b1589784ea14fc46c
Laxika <laxika91@gmail.com>
2019-07-01 22:58:48 +0200

    Added jacoco.

---
+++ java-task-force-core/src/test/java/com/morethanheroic/taskforce/executor/JobExecutorTest.java
@@ -0,0 +1,12 @@
+package com.morethanheroic.taskforce.executor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JobExecutorTest {
+
+    @Test
+    public void execute() {
+    }
+}
